eaef6565f7ebf0cf86e9096e3265d102f22308a2
Eren Gazioglu <pompyproduction@gmail.com>
2023-05-25 00:12:32 +0200

    Refine webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,18 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "dist"),
-        clean: true
+        clean: true,
+        assetModuleFilename: "[name][ext]"
     },
     module: {
         rules: [
             {
-                test: /\.s[ac]ss$/,
+                test: /\.s[ac]ss$/i,
                 use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(jpe?g|png|svg|gif)$/i,
+                type: "asset/resource"
             }
         ]
     },
@@ -26,8 +31,12 @@ module.exports = {
             template: path.resolve(__dirname, "src/template.html")
         })
     ],
+    devtool: "source-map",
     devServer: {
-        static: path.resolve(__dirname, "dist")
+        static: path.resolve(__dirname, "dist"),
+        historyApiFallback: true,
+        open: true,
+        hot: true
     },
     // optimization: {
     //     runtimeChunk: 'single',
