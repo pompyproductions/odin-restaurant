e9c01ba238ef3bb7d9e4de40ad5f6f9f0754e667
Eren Gazioglu <pompyproduction@gmail.com>
2023-06-09 00:27:17 +0200

    Make functional nav

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import domalt from "domalt";
 import { getPage } from "./modules/pages.js";
 
 const entryPoint = document.getElementById("entry");
+const navElems = ["home", "menu", "contact", "test"].map((pageName) => {
+  return {
+    tag: "a",
+    content: pageName,
+    listeners: [
+      ["click", () => switchPage(pageName)]
+    ]
+  }
+});
 
 entryPoint.append(
   domalt.newElem({
@@ -14,7 +23,10 @@ entryPoint.append(
       },
     ],
   }),
-  // domalt.newElemNav(),
+  domalt.newElem({
+    tag: "nav",
+    children: navElems
+  }),
   domalt.newElem({
     tag: "main",
     saveAs: "content",
@@ -30,6 +42,8 @@ entryPoint.append(
   })
 );
 
-const pageNav = document.querySelector("nav.page-nav");
-const pageContent = document.getElementById("content");
-domalt.retrieve("content").replaceChildren(getPage("home"));
+const switchPage = (pageName) => {
+  domalt.retrieve("content").replaceChildren(getPage(pageName));
+}
+
+switchPage("home");
